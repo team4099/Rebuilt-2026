fcb5e359621254b5a774971ac8cceaf09ebd6b17
SirBeans <ryanchung0084@gmail.com>
2026-01-25 20:36:10 -0500

    remove stuff

--- src/main/kotlin/com/team4099/robot2026/config/constants/RollersConstants.kt
+++ src/main/kotlin/com/team4099/robot2026/config/constants/RollersConstants.kt
@@ -11,9 +11,6 @@ object RollersConstants {
   val GEAR_RATIO = 1.0 / 1.0
   val VOLTAGE_COMPENSATION = 12.volts
 
-  val FUEL_CURRENT_THRESHOLD = 30.0.amps
-  val FUEL_DETECTION_THRESHOLD = 2.0.seconds
-
   val IDLE_VOLTAGE = 0.0.volts
   val EJECT_VOLTAGE = (-3.0).volts
   val INTAKE_VOLTAGE = 6.0.volts
