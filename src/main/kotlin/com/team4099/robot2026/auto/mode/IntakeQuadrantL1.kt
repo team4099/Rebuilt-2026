53a44cf65a65190b86d89c8bb189a0ee6b7a69ed
ImJustinLau <jlau@team4099.com>
2026-02-16 12:02:43 -0500

    ran spotless apply

--- src/main/kotlin/com/team4099/robot2026/auto/mode/IntakeQuadrantL1.kt
+++ src/main/kotlin/com/team4099/robot2026/auto/mode/IntakeQuadrantL1.kt
@@ -1,6 +1,5 @@
 package com.team4099.robot2026.auto.mode
 
-import com.ctre.phoenix6.swerve.SwerveDrivetrain
 import com.team4099.robot2026.commands.drivetrain.FollowChoreoPath
 import com.team4099.robot2026.subsystems.drivetrain.Drive
 import com.team4099.robot2026.subsystems.superstructure.Superstructure
@@ -8,20 +7,20 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.WaitCommand
 
-class IntakeQuadrantL1(val drivetrain: Drive, val superstructure: Superstructure) : SequentialCommandGroup(){
+class IntakeQuadrantL1(val drivetrain: Drive, val superstructure: Superstructure) :
+    SequentialCommandGroup() {
   init {
     addRequirements(drivetrain, superstructure)
     addCommands(
-      ParallelCommandGroup(
-        FollowChoreoPath(),
-        WaitCommand(1.3).andThen(superstructure.requestIntakeCommand()),
-        WaitCommand(2.5).andThen(superstructure.requestIntakeCommand()),
-
-      ),
-      superstructure.requestScoreCommand(),
-      WaitCommand(10.0), // should i use a constant here
-      //superstructure.requestPrepClimbCommand(),
-      //superstructure.requestClimbCommand()
+        ParallelCommandGroup(
+            FollowChoreoPath(),
+            WaitCommand(1.3).andThen(superstructure.requestIntakeCommand()),
+            WaitCommand(2.5).andThen(superstructure.requestIntakeCommand()),
+        ),
+        superstructure.requestScoreCommand(),
+        WaitCommand(10.0), // should i use a constant here
+        // superstructure.requestPrepClimbCommand(),
+        // superstructure.requestClimbCommand()
     )
   }
 }
