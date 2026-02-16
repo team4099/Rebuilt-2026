4dca524e223d93bd1f8106c6d98c1fc6ef6ad59a
Shaunak Kumar <write2shaunak@gmail.com>
2026-02-16 06:39:53 -0500

    disruption auto - 10 seconds no climb no shoot almost all balls in mid will b displaced

---
+++ src/main/kotlin/com/team4099/robot2026/auto/mode/DisruptionAuto.kt
@@ -0,0 +1,31 @@
+package com.team4099.robot2026.auto.mode
+
+import choreo.Choreo
+import choreo.trajectory.SwerveSample
+import com.team4099.robot2026.RobotContainer.superstructure
+import com.team4099.robot2026.commands.drivetrain.FollowChoreoPath
+import com.team4099.robot2026.subsystems.drivetrain.Drive
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
+import org.team4099.lib.geometry.Pose2d
+
+class DisruptionAuto(val drivetrain: Drive) : SequentialCommandGroup() {
+  init {
+    addRequirements(drivetrain)
+
+    addCommands(
+        ParallelCommandGroup(
+            FollowChoreoPath(drivetrain, firstTrajectory),
+            WaitCommand(.75)
+                .andThen(
+                    superstructure.requestForceIntakeDownCommand(),
+                    WaitCommand(.9).andThen(superstructure.requestIntakeCommand()))))
+  }
+
+  companion object {
+    val firstTrajectory = Choreo.loadTrajectory<SwerveSample>("disruption/NewPath.traj").get()
+
+    val startingPose = Pose2d(firstTrajectory.getInitialPose(false).get())
+  }
+}
