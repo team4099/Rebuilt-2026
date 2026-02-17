318b122d66c542ae06a9d174941270efe052599b
ryx731 <rshih@team4099.com>
2026-02-16 22:37:47 -0500

    preload + l1 climb auto

---
+++ src/main/kotlin/com/team4099/robot2026/auto/mode/PreloadL1Auto.kt
@@ -0,0 +1,45 @@
+package com.team4099.robot2026.auto.mode
+
+import choreo.Choreo
+import choreo.trajectory.SwerveSample
+import com.team4099.robot2026.commands.drivetrain.FollowChoreoPath
+import com.team4099.robot2026.subsystems.drivetrain.Drive
+import com.team4099.robot2026.subsystems.superstructure.Request
+import com.team4099.robot2026.subsystems.superstructure.Superstructure
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
+import org.team4099.lib.geometry.Pose2d
+
+class PreloadL1Auto(val drivetrain: Drive, val superstructure: Superstructure) : SequentialCommandGroup() {
+    init {
+        addRequirements(drivetrain)
+
+        addCommands(
+            FollowChoreoPath(drivetrain, firstTrajectory),
+            ParallelCommandGroup(
+                WaitCommand(2.0),
+                superstructure.requestScoreCommand()
+            ),
+            ParallelCommandGroup(
+                FollowChoreoPath(drivetrain, secondTrajectory),
+                superstructure.requestPrepClimbCommand()
+            ),
+            superstructure.requestClimbCommand()
+        )
+    }
+
+    companion object {
+        val firstTrajectory =
+            Choreo.loadTrajectory<SwerveSample>("preload/preloadauto.traj").get()
+        // prep shoot
+
+        val secondTrajectory =
+            Choreo.loadTrajectory<SwerveSample>("preload/NewPath.traj").get()
+
+        // shoot
+        // don't flip pose: poses are robot relative since field frame estimator was reset
+        val startingPose = Pose2d(firstTrajectory.getInitialPose(false).get())
+    }
+}
