1b3e4583b13efcf44e95942b201faf95291f4e24
Sammy <sammy.adjones@gmail.com>
2026-02-25 09:37:33 -0500

    spotlessapply

--- src/main/kotlin/com/team4099/robot2026/RobotContainer.kt
+++ src/main/kotlin/com/team4099/robot2026/RobotContainer.kt
@@ -254,8 +254,7 @@ object RobotContainer {
 
   fun mapTunableCommands() {}
 
-  fun getAutonomousCommand() =
-      AutonomousSelector.getCommand(drivetrain, vision, superstructure)
+  fun getAutonomousCommand() = AutonomousSelector.getCommand(drivetrain, vision, superstructure)
 
   fun resetSimulationField() {
     if (!RobotBase.isSimulation()) return
