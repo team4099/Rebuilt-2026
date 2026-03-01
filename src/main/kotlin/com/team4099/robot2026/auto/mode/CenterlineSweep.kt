7b419b7a4a9c94cc61b1f9cb6470f614e3fcd162
SirBeans <ryanchung0084@gmail.com>
2026-02-28 23:30:18 -0500

    centerline sweep

--- src/main/kotlin/com/team4099/robot2026/auto/mode/CenterlineSweep.kt
+++ src/main/kotlin/com/team4099/robot2026/auto/mode/CenterlineSweep.kt
@@ -2,6 +2,7 @@ package com.team4099.robot2026.auto.mode
 
 import choreo.Choreo
 import choreo.trajectory.SwerveSample
+import com.team4099.robot2026.auto.mode.IntakeQuadrantL1.Companion.climbFlippedTraj
 import com.team4099.robot2026.commands.drivetrain.AimOTFCommand
 import com.team4099.robot2026.commands.drivetrain.FollowChoreoPath
 import com.team4099.robot2026.config.constants.IntakeConstants
@@ -17,25 +18,24 @@ import edu.wpi.first.wpilibj2.command.WaitUntilCommand
 import org.team4099.lib.geometry.Pose2d
 import org.team4099.lib.units.base.seconds
 
-class IntakeQuadrantL1(
-    val drivetrain: Drive,
-    val superstructure: Superstructure,
-    flipVeritcally: Boolean
+class CenterlineSweep(
+  val drivetrain: Drive,
+  val superstructure: Superstructure,
+  flipVeritcally: Boolean
 ) : SequentialCommandGroup() {
   init {
     addRequirements(drivetrain, superstructure)
     addCommands(
       SequentialCommandGroup(
         ParallelCommandGroup(
-            FollowChoreoPath(drivetrain, mainTraj, flipVertically = flipVeritcally),
-            SequentialCommandGroup(
-                superstructure.requestIntakeCommand(),
-                WaitCommand(6.5),
-                superstructure.requestIdleCommand(),
-              superstructure.requestPrepScoreCommand())
-            ),
+          FollowChoreoPath(drivetrain, mainTraj, flipVertically = flipVeritcally),
+          SequentialCommandGroup(
+            superstructure.requestIntakeCommand(),
+            WaitCommand(3.0),
+            superstructure.requestIdleCommand(),
+            superstructure.requestPrepScoreCommand())
+        ),
         superstructure.requestScoreCommand(),
-//        AimOTFCommand(drivetrain, 1.0.seconds),
         WaitUntilCommand({superstructure.currentState == Superstructure.Companion.SuperstructureStates.SCORE}),
         WaitCommand(1.0),
         RepeatCommand(
@@ -45,7 +45,7 @@ class IntakeQuadrantL1(
             superstructure.requestForceIntakeCommand(IntakeConstants.ANGLES.FORCE_DOWN_ANGLE),
             WaitCommand(0.1)
           )
-        ).withTimeout(3.0),
+        ).withTimeout(4.0),
         RepeatCommand(
           SequentialCommandGroup(
             superstructure.requestForceIntakeCommand(IntakeConstants.ANGLES.FORCE_HALFUP_ANGLE),
@@ -53,13 +53,12 @@ class IntakeQuadrantL1(
             superstructure.requestForceIntakeCommand(IntakeConstants.ANGLES.FORCE_HALFDOWN_ANGLE),
             WaitCommand(0.1)
           )
-        ).withTimeout(3.0),
+        ).withTimeout(4.0),
         superstructure.requestIdleCommand(),
         WaitCommand(1.0),
       ).withTimeout(12.0),
       superstructure.requestIdleCommand(),
       superstructure.requestPrepClimbCommand(),
-      WaitCommand(1.5),
       ConditionalCommand(
         FollowChoreoPath(drivetrain, climbFlippedTraj),
         InstantCommand()
@@ -70,9 +69,9 @@ class IntakeQuadrantL1(
 
   companion object {
     val mainTraj =
-        Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/IntakeQuadrantClimb.traj").get()
+      Choreo.loadTrajectory<SwerveSample>("SweepCenter/MidlineSweep.traj").get()
 
-    val climbFlippedTraj = Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/ClimbFlipped.traj").get()
+    val climbFlippedTraj = Choreo.loadTrajectory<SwerveSample>("SweepCenter/Climb.traj").get()
 
     val startingPose = Pose2d(mainTraj.getInitialPose(false).get())
   }
