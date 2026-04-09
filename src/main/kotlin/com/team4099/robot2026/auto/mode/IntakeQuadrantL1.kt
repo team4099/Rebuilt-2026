a354e66e195b75b4181bc85f666afb2b8a5e6869
SirBeans <ryanchung0084@gmail.com>
2026-04-09 17:08:04 -0400

    spotlessapply

--- src/main/kotlin/com/team4099/robot2026/auto/mode/IntakeQuadrantL1.kt
+++ src/main/kotlin/com/team4099/robot2026/auto/mode/IntakeQuadrantL1.kt
@@ -13,48 +13,41 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.WaitCommand
 import org.team4099.lib.geometry.Pose2d
-import org.team4099.lib.units.base.inSeconds
 import org.team4099.lib.units.base.seconds
 
 class IntakeQuadrantL1(
-  val drivetrain: Drive,
-  val superstructure: Superstructure,
-  val intake: Intake,
-  flipVeritcally: Boolean
+    val drivetrain: Drive,
+    val superstructure: Superstructure,
+    val intake: Intake,
+    flipVeritcally: Boolean
 ) : SequentialCommandGroup() {
   init {
     addCommands(
-      ParallelCommandGroup(
-        FollowChoreoPath(
-          drivetrain,
-          mainTraj,
-          flipVertically = flipVeritcally,
-          interruptAtTimeout = true
-        ),
-        SequentialCommandGroup(
-          WaitCommand(0.75),
-          superstructure.requestIntakeCommand(),
-          WaitCommand(5.25),
-          superstructure.requestPrepScoreCommand(),
-          ParallelCommandGroup(
-            AimOTFCommand(drivetrain, timeout = 15.seconds),
+        ParallelCommandGroup(
+            FollowChoreoPath(
+                drivetrain, mainTraj, flipVertically = flipVeritcally, interruptAtTimeout = true),
             SequentialCommandGroup(
-              WaitCommand(1.0),
-              superstructure.requestScoreCommand(),
-              AgitateIntakeCommand(superstructure, intake).withTimeout(18.0 - (5.25 + 0.75 + 1.0)),
-              superstructure.requestForceIntakeCommand(IntakeConstants.ANGLES.FORCE_HALFUP_ANGLE)
-            )
-          )
-        ),
-      )
-    )
+                WaitCommand(0.75),
+                superstructure.requestIntakeCommand(),
+                WaitCommand(5.25),
+                superstructure.requestPrepScoreCommand(),
+                ParallelCommandGroup(
+                    AimOTFCommand(drivetrain, timeout = 15.seconds),
+                    SequentialCommandGroup(
+                        WaitCommand(1.0),
+                        superstructure.requestScoreCommand(),
+                        AgitateIntakeCommand(superstructure, intake)
+                            .withTimeout(18.0 - (5.25 + 0.75 + 1.0)),
+                        superstructure.requestForceIntakeCommand(
+                            IntakeConstants.ANGLES.FORCE_HALFUP_ANGLE)))),
+        ))
   }
 
   companion object {
     val mainTraj =
-      Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/IntakeQuadrantClimbQuick.traj").get()
+        Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/IntakeQuadrantClimbQuick.traj").get()
     val secondSwipeTraj =
-      Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/BackToQuadrant.traj").get()
+        Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/BackToQuadrant.traj").get()
 
     val startingPose = Pose2d(mainTraj.getInitialPose(false).get())
   }
--- src/main/kotlin/com/team4099/robot2026/auto/mode/IntakeSideSpin.kt
+++ src/main/kotlin/com/team4099/robot2026/auto/mode/IntakeSideSpin.kt
@@ -15,41 +15,32 @@ import org.team4099.lib.geometry.Pose2d
 import org.team4099.lib.units.base.seconds
 
 class IntakeSideSpin(
-  val drivetrain: Drive,
-  val superstructure: Superstructure,
-  val intake: Intake,
-  flipVeritcally: Boolean
+    val drivetrain: Drive,
+    val superstructure: Superstructure,
+    val intake: Intake,
+    flipVeritcally: Boolean
 ) : SequentialCommandGroup() {
   init {
     addCommands(
-      ParallelCommandGroup(
-        FollowChoreoPath(
-          drivetrain,
-          mainTraj,
-          flipVertically = flipVeritcally,
-          interruptAtTimeout = true
-        ),
-        SequentialCommandGroup(
-          WaitCommand(0.75),
-          superstructure.requestIntakeCommand(),
-          WaitCommand(5.5),
-          superstructure.requestPrepScoreCommand(),
-          ParallelCommandGroup(
-            AimOTFCommand(drivetrain, timeout = 15.seconds),
+        ParallelCommandGroup(
+            FollowChoreoPath(
+                drivetrain, mainTraj, flipVertically = flipVeritcally, interruptAtTimeout = true),
             SequentialCommandGroup(
-              WaitCommand(0.5),
-              superstructure.requestScoreCommand(),
-              AgitateIntakeCommand(superstructure, intake)
-            )
-          )
-        )
-      )
-    )
+                WaitCommand(0.75),
+                superstructure.requestIntakeCommand(),
+                WaitCommand(5.5),
+                superstructure.requestPrepScoreCommand(),
+                ParallelCommandGroup(
+                    AimOTFCommand(drivetrain, timeout = 15.seconds),
+                    SequentialCommandGroup(
+                        WaitCommand(0.5),
+                        superstructure.requestScoreCommand(),
+                        AgitateIntakeCommand(superstructure, intake))))))
   }
 
   companion object {
     val mainTraj =
-      Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/IntakeQuadrantSpin.traj").get()
+        Choreo.loadTrajectory<SwerveSample>("IntakeQuadrantL1/IntakeQuadrantSpin.traj").get()
 
     val startingPose = Pose2d(mainTraj.getInitialPose(false).get())
   }
