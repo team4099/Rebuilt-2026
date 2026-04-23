c99cbca11b95555716c3ac3f218863476d6835e5
smartypants0506 <omkar.bantanur@gmail.com>
2026-04-23 17:38:12 -0400

    fixing what was wabi sabi

--- src/main/kotlin/com/team4099/robot2026/util/driver/DriverProfile.kt
+++ src/main/kotlin/com/team4099/robot2026/util/driver/DriverProfile.kt
@@ -14,9 +14,8 @@ abstract class DriverProfile(
     private val sensitivityDrivePowerConstant: Int,
     private val sensitivityRotationPowerConstant: Int,
     private val driveSlowModeClamp: Double = 0.75,
-    private val turnSlowModeClamp: Double =
-        0.75 private
-            val fieldRelativeControls: Boolean = true
+    private val turnSlowModeClamp: Double = 0.75, 
+    private val fieldRelativeControls: Boolean = true
 ) {
   private val invertDriveMultiplier = if (invertDrive) -1 else 1
   private val invertRotationMultiplier = if (invertRotation) -1 else 1
